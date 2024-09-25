f152543b64165c73d6da888e3acaafeeeaaeef89
jinfan <15868141213@163.com>
2024-09-25 17:52:07 +0800

    新增yolov5模型onnxruntime python 推理代码,输入带anchor

--- yolov5_onnx_with_anchor_py/main.py
+++ yolov5_onnx_with_anchor_py/main.py
@@ -8,8 +8,8 @@ from detect_utils import detect
 detect_utils.OBJ_THRESH = 0.6
 detect_utils.NMS_THRESH = 0.25
 size = (544, 960)
-detect_onnx = "/home/jinfan/Desktop/ai_tt2/deploy_detect_o.O/checkpoints/pc_yolov5s.onnx"
-image_path = "/home/jinfan/Desktop/ai_tt2/deploy_detect_o.O/data/9.jpg"
+detect_onnx = "./checkpoints/pc_yolov5s.onnx"
+image_path = "./data/9.jpg"
 anchors = [[26, 26], [56, 16], [116, 19], [235, 23], [540, 32],
            [139, 139], [413, 64], [364, 292], [479, 703]]
 detect_session = onnxruntime.InferenceSession(detect_onnx)
